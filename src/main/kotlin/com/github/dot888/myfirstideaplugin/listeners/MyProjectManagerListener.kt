36aacb954709be0c998827adefa75cfc46a9398d
GitHub Action <action@github.com>
2021-11-02 11:35:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/dot888/myfirstideaplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/dot888/myfirstideaplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.dot888.myfirstideaplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.dot888.myfirstideaplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/dot888/myfirstideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/dot888/myfirstideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.dot888.myfirstideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dot888.myfirstideaplugin.MyBundle
 
 class MyApplicationService {
 
